12d2a70bde99f84b59458f501449ac39a528a12f
Olaoye <oyekunlejohnson8@gmail.com>
2022-11-14 00:08:09 +0100

    implementation of the Produc service and the Order service

---
+++ order-service/src/main/java/com/olaoye/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,39 @@
+package com.olaoye.orderservice.service;
+
+import com.olaoye.orderservice.dto.OrderLineItemsDto;
+import com.olaoye.orderservice.dto.OrderRequest;
+import com.olaoye.orderservice.model.Order;
+import com.olaoye.orderservice.model.OrderLineItems;
+import com.olaoye.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    public void placeOrder (OrderRequest orderRequest){
+        Order order = new Order();
+       List<OrderLineItems>  orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+       order.setOrderNumber(UUID.randomUUID().toString().split("-")[0]);
+       order.setOrderLineItemsList(orderLineItems);
+       orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        return orderLineItems;
+    }
+}
