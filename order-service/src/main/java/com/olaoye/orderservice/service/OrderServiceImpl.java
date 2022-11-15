9d73e837bbc0b2d4b27b162f67ee31d0ea5fb50b
Olaoye <oyekunlejohnson8@gmail.com>
2022-11-15 22:13:32 +0100

    added transaction annotation to the order service class

--- order-service/src/main/java/com/olaoye/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/olaoye/orderservice/service/OrderServiceImpl.java
@@ -7,12 +7,14 @@ import com.olaoye.orderservice.model.OrderLineItems;
 import com.olaoye.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
